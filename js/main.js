f0516cd2f1dbfddb29536a2b5c3f359a0986ae5f
Mihai Dogaru <dogarumihai2537@gmail.com>
2026-03-26 09:37:08 +0200

    Start carousel skel

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ console.log("Hello world!");
 const btnNavEl = document.querySelector(".btn--mobile-nav");
 const navEl = document.querySelector("nav");
 
+const btnPrevMobile = document.getElementById("carouselBtnPrevMobile");
+const btnNextMobile = document.getElementById("carouselBtnNextMobile");
+
 btnNavEl.addEventListener("click", () => {
   navEl.dataset.open = navEl.dataset.open === "true" ? "false" : true;
   document.body.classList.toggle("no-scroll");
+});
+
+btnPrevMobile.addEventListener("click", () => {
+  console.log("Prev clicked");
+});
+
+btnNextMobile.addEventListener("click", () => {
+  console.log("Next clicked");
 });
